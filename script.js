2db661f420bad30223c300c570b1fd868baa2159
Konstantin Stark <konstantin.stark.dev@gmail.com>
2024-09-30 15:23:26 +0200

    added poke data and some styles

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
-
-
-
 function init() {
-    fetchDataText();
+    fetchPokemonData();
 }
 
-async function fetchDataText() {
-    let response = await fetch('');
-
-    let responseAsText = await response.json();
-    console.log(responseAsText);
+async function fetchPokemonData() {
+    let pokemon = await fetch('https://pokeapi.co/api/v2/pokemon?limit=1&offset=0');
+    let pokemonContent = await pokemon.json();
+    console.log(pokemonContent);
 
     let htmlContent = document.getElementById('content');
-    for (let index = 0; index < responseAsText.length; index++) {
-        let element = responseAsText[index];
+    let results = pokemonContent.results;
+
+    for (let index = 0; index < results.length; index++) {
+
+        let element = results[index];
+        let pokemonDetails = await fetch(element.url);
+        let pokemonData = await pokemonDetails.json();
+        console.log(pokemonData);
+
         htmlContent.innerHTML += /*html*/`
-        <div class="gotChars">
-            <h1>${element.fullName}</h1>
-            <h2>${element.title}</h2>
-        </divY>
+        
+        <div class="poke-content">
+            <div class="poke-cards">
+                <div class="poke-char">
+                    <h1>${element.name}</h1>
+                    <img class="poke-char-img" src="${pokemonData.sprites.other['official-artwork'].front_default}">
+                    <p>${pokemonData[index]}</p>
+                </div>
+            </div>
+        </div>
     `
     }
 }
+
