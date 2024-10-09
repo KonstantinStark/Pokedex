cbb5a5cb9dc38956f8c5e0204d14f8c29ecbdb7c
Konstantin Stark <konstantin.stark.dev@gmail.com>
2024-10-09 08:34:23 +0200

    clean code

--- js/search.js
+++ js/search.js
@@ -20,32 +20,19 @@ function searchPokemon() {
 
 function handleFilteredPokemon(filteredPokemon) {
     const mainContainer = document.querySelector('.main-container');
-    document.getElementById('content').innerHTML = ''; // Clear previous content
-
+    document.getElementById('content').innerHTML = '';
     if (filteredPokemon.length === 0) {
         const noResultsMessage = document.createElement('div');
         noResultsMessage.className = 'no-results';
         noResultsMessage.textContent = 'no Pokémon found';
         document.getElementById('content').appendChild(noResultsMessage);
-        mainContainer.style.height = '100vh'; // Set height to 100vh when no Pokémon found
+        mainContainer.style.height = '100vh';
     } else {
-        mainContainer.style.height = '100vh'; // Set height to 100vh when Pokémon are found
+        mainContainer.style.height = '100vh';
         filteredPokemon.forEach((pokemon) => {
             let originalIndex = pokemonArray.findIndex(p => p.id === pokemon.id);
             document.getElementById('content').innerHTML += generatePokemonCard(pokemon, pokemon, originalIndex);
             renderTypes(pokemon, pokemon, originalIndex);
         });
     }
-}
-
-async function loadMorePokemon() {
-    const loadButton = document.querySelector('.load-button-style');
-    loadButton.style.display = 'none';
-    document.getElementById('loading').style.display = 'block';
-    document.getElementById('content').classList.add('loading');
-    offset += 15;
-    await fetchPokemonData(offset);
-    document.getElementById('loading').style.display = 'none';
-    document.getElementById('content').classList.remove('loading');
-    loadButton.style.display = 'block';
 }
